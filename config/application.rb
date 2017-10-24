584e51bec285556788ce6afcc3d90c7068465552
Max Melentiev <melentievm@gmail.com>
2017-10-24 11:35:45 +0300

    Rubocop, cleenup

--- config/application.rb
+++ config/application.rb
@@ -1,20 +1,21 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+# require "action_cable/engine"
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Bundler.require(:assets) if Rails.env.in?(%w[development test])
 
 module BasicRailsApp
   class Application < Rails::Application
